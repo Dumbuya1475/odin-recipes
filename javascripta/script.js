7eebed89fb195e4c1cc9e26d779e9fce88257470
Mohamed Super Dumbuya <dumbuya366@gmail.com>
2024-02-08 14:49:55 +0100

    update_file

---
+++ javascripta/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+
+function greet() {
+  const name = prompt("What is your name?");
+  const greeting = document.querySelector("#greeting");
+  greeting.textContent = `Hello ${name}, nice to see you!`;
+}
+
+button.addEventListener("click", greet);#
